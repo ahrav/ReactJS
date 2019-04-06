813a922fe859672ad7f0d9f361cf74c75e586968
Ahrav Dutta <ahravdutta02@gmail.com>
2019-04-06 05:26:10 -0700

    Burger builder app project

---
+++ burgerApp/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Aux from '../../../hoc/Aux'
+import Button from '../../UI/Button/Button'
+
+const orderSummary = props => {
+    const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+        return (
+            <li><span key={ingKey} style={{textTransform: 'capitalize'}}>{ingKey}: {props.ingredients[ingKey]}</span></li>
+        )
+    })
+    return (
+        <Aux>
+            <h3>Your Order</h3>
+            <p>A delicious burger with the following ingredients</p>
+            <ul>
+                {ingredientSummary}
+            </ul>
+            <p><strong>Total Price: ${props.totalPrice.toFixed(2)}</strong></p>
+            <p>Continue to checkout?</p>
+            <Button btnType="Danger" clicked={props.purchaseCanceled} > CANCEL </Button>
+            <Button clicked={props.purchaseContinue} btnType="Success"> CONTINUEs </Button>
+        </Aux>
+    )
+};
+
+export default orderSummary;
