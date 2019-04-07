5a1f690a0e9c461903750c0e7f3dc86ea976cffe
Ahrav Dutta <ahravdutta02@gmail.com>
2019-04-07 01:35:26 -0700

    Update directory structure

--- burgerApp/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burgerApp/burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,26 +1,32 @@
-import React from 'react'
-import Aux from '../../../hoc/Aux'
+import React, { Component } from 'react'
+import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
-const orderSummary = props => {
-    const ingredientSummary = Object.keys(props.ingredients).map(ingKey => {
+class OrderSummary extends Component{
+    componentDidUpdate() {
+        console.log("[OrderSummary] will update")
+    }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients).map(ingKey => {
+            return (
+                <li key={ingKey}><span  style={{textTransform: 'capitalize'}}>{ingKey}: {this.props.ingredients[ingKey]}</span></li>
+            )
+        })
         return (
-            <li key={ingKey}><span  style={{textTransform: 'capitalize'}}>{ingKey}: {props.ingredients[ingKey]}</span></li>
-        )
-    })
-    return (
-        <Aux>
+            <Aux>
             <h3>Your Order</h3>
             <p>A delicious burger with the following ingredients</p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: ${props.totalPrice.toFixed(2)}</strong></p>
+            <p><strong>Total Price: ${this.props.totalPrice.toFixed(2)}</strong></p>
             <p>Continue to checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCanceled} > CANCEL </Button>
-            <Button clicked={props.purchaseContinue} btnType="Success"> CONTINUEs </Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCanceled} > CANCEL </Button>
+            <Button clicked={this.props.purchaseContinue} btnType="Success"> CONTINUEs </Button>
         </Aux>
-    )
-};
+        );
+    }
+} 
 
-export default orderSummary;
+export default OrderSummary;
--- burgerApp/burger-builder/src/components/UI/Modal/Modal.js
+++ burgerApp/burger-builder/src/components/UI/Modal/Modal.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import classes from './Modal.css'
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux/Aux'
 import Backdrop from '../Backdrop/Backdrop'
 
-const modal = props => (
-    <Aux>
-        <Backdrop clicked={props.modalClosed} show={props.show} />
-        <div className={classes.Modal}
-        style={{
-            transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-            opacity: props.show ? '1' : '0'
-        }}>
-            {props.children}
-        </div>
-    </Aux>
-);
+const modal = React.memo(props => {
+    if (props.show){
+        return (
+            <Aux>
+                <Backdrop clicked={props.modalClosed} show={props.show} />
+                <div className={classes.Modal}
+                style={{
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }}>
+                    {props.children}
+                </div>
+            </Aux>
+        )
+    }
+})
 
 export default modal;
