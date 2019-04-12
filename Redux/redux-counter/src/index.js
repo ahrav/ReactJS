73cb7dea0ed131e78040071f016c79b48e949610
Ahrav Dutta <ahravdutta02@gmail.com>
2019-04-11 23:16:38 -0700

    Add redux-thunk to application to include asynchronous features

--- Redux/redux-counter/src/index.js
+++ Redux/redux-counter/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -13,7 +14,20 @@ const rootReducer = combineReducers({
     res: resultsReducer
 });
 
-const store = createStore(rootReducer);
+const logger = store => {
+    return next => {
+        return action => {
+            console.log('MIddleware Dispatching', action);
+            const result = next(action);
+            console.log('Middleware next state', store.getState());
+            return result;
+        }
+    }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger, thunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 serviceWorker.unregister();
