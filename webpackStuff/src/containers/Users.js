f455472420be5af30c5e32eb4c9620bfb36d0799
Ahrav Dutta <ahravdutta02@gmail.com>
2019-04-13 03:00:22 -0700

    webpack config setup

---
+++ webpackStuff/src/containers/Users.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import PizzaImage from '../components//PizzaImage/PizzaImage';
+
+class Users extends Comment {
+    render() {
+        return (
+            <div>
+                <h1>The Users</h1>
+                <p>Awesome stuff I think??</p>
+            </div>
+        );
+    }
+}
+
+export default Users;
